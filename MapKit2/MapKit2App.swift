8941a0225459ca6e2ac74ec21e400a7e33bd8f01
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-18 16:34:26 +0400

    Initial Commit

---
+++ MapKit2/MapKit2App.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit2App.swift
+//  MapKit2
+//
+//  Created by Emmanuelle  Dennemont on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapKit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
